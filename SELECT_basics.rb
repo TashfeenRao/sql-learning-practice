4c5ad93297d210613229094a13c3d4d9b9018bac
TashfeenRao <raotashfeen25@gmail.com>
2020-01-17 17:12:19 +0500

    select basics quiz

--- SELECT_basics.rb
+++ SELECT_basics.rb
@@ -6,3 +6,18 @@
 
   SELECT name, area FROM world
   WHERE area BETWEEN 200000 AND 250000
+
+  SELECT name, population
+  FROM world
+  WHERE name LIKE "Al%"
+
+  SELECT name FROM world
+  WHERE name LIKE '%a' OR name LIKE '%l'
+
+  SELECT name, population/area
+  FROM world
+  WHERE name IN ('China', 'Nigeria', 'France', 'Australia'
+
+  SELECT name, area, population
+  FROM world
+  WHERE area > 50000 AND population < 10000000
